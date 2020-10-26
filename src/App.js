5e9ea645d9ca0c71aaa5252bbb2230990e1637e1
dhb220 <dhb220@gmail.com>
2020-10-25 23:01:11 -0500

    change app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          First React App
+          Move to Feature branch
         </p>
         <a
           className="App-link"
